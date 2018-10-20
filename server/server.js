fe55ab1738b710b2228c3edd36bb55e459049ffa
Udayaditya Singh <udayaditya.singh@gmail.com>
2018-10-20 14:09:20 +0530

    First

---
+++ server/server.js
@@ -0,0 +1,64 @@
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+var Todo = mongoose.model('Todo', {
+    text: {
+        type:String,
+        required:true,
+        minlength:1,
+        trim: true
+    },
+    completed: {
+        type:Boolean,
+        default:false
+    },
+    completedAt: {
+        type:Number,
+        default:null
+    }
+});
+
+var newTodo = new Todo({
+    text: '  Edit the part of video   '
+});
+
+newTodo.save().then((doc) => {
+    console.log('Saved Todo',doc);
+}, (err) => {
+    console.log('Unable to save todo',err);
+});
+
+var otherTodo = new Todo({
+    text: 'Feed at cat',
+    completed: true,
+    completedAt:123
+});
+
+otherTodo.save().then((doc) => {
+    console.log(JSON.stringify(doc, undefined, 2));   
+}, (err) => {
+    console.log('Unable to save',err);
+});
+
+
+var User = mongoose.model('User', {
+    email: {
+        type:String,
+        required:true,
+        trim:true,
+        minlength:1
+    }
+});
+
+var user = new User({
+    email: '   MY EMAIL   '
+});
+
+user.save().then((doc) => {
+    console.log('User Saved',doc);
+}, (err) => {
+    console.log('Unable to save user',err);
+    
+})
