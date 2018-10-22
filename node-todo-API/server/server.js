fad8b189322d5a70ad73b4d3777b2cd4bb8a1fed
Udayaditya Singh <udayaditya.singh@gmail.com>
2018-10-22 23:47:12 +0530

    Get by id

--- node-todo-API/server/server.js
+++ node-todo-API/server/server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb'); 
+
 var {mongoose} = require('./db/moongose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -26,7 +28,26 @@ app.get('/todos', (req, res) =>{
     }, (error) => {
         res.status(400).send(e);
     })
-})
+});
+
+app.get('/todos/:id', (req, res) =>{
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        console.log('Invalid Object ID');
+        return res.status(404).send({});
+    }
+
+    Todo.findById(id).then((todo) => {
+        if(!todo){
+            console.log(`Doeesn't find any data`);
+            return res.status(404).send({});
+        }
+        res.send({todo});
+    }).catch((e) => {
+        console.log('Got Some Error');
+        res.status(400).send({});
+    })
+});
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
