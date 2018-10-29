1626d58472acad55ef05ee0d4482f8240c8c5e42
Udayaditya Singh <udayaditya.singh@gmail.com>
2018-10-29 16:26:42 +0530

    app for heroku

--- node-todo-API/server/db/moongose.js
+++ node-todo-API/server/db/moongose.js
@@ -1,6 +1,16 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }, 
+                replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };
+var mongodbUri = 'mongodb://uday:uday123456@ds145043.mlab.com:45043/node-app';
+mongoose.connect(mongodbUri, options);
+var conn = mongoose.connection; 
 
+conn.on('error', console.error.bind(console, 'connection error:'));  
+
+conn.once('open', () => {
+    console.log('Successfully Connected');
+    
+});
 module.exports = { mongoose };
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://udayaditya.singh:12345678@ds145043.mlab.com:45043/node-app');
 
 var Todo = mongoose.model('Todo', {
     text: {
